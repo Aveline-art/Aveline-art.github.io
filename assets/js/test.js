004fa523c00178e9652456491dff37c6bc203b28
aveline-art <62368440+Aveline-art@users.noreply.github.com>
2021-11-02 21:31:23 -0700

    added all links

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,7 +1,7 @@
 const links = {
     0: 'https://docs.google.com/forms/d/e/1FAIpQLScSTXKbJy425-kSCaylG8n49TpN9bxUECRSwWke9tfQRnHqSA/viewform',
-    1: 'https://www.youtube.com/',
-    2: 'https://www.google.com/',
+    1: 'https://docs.google.com/forms/d/e/1FAIpQLScPR4yOxnYtk9pxA9nA0IQdYxqHgAcCoS1O6gT536eOA1rytw/viewform',
+    2: 'https://docs.google.com/forms/d/e/1FAIpQLSeldvsdfAZU1BViLBR1su7_uk_0nuOCDJuJfHd4R4RYCAuHZA/viewform',
 }
 
 function getRandomInt(min, max) {
